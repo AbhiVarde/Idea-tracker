b454af92a2dd3f6b7f63c6a63b6209784c1a703c
AbhiVarde <abhivarde99@gmail.com>
2025-06-13 10:44:24 +0530

    fix: env issue

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,5 +1,10 @@
 import { Client, Databases, Account } from "appwrite";
 
+// Debug logs
+console.log("Environment variables:");
+console.log("Project ID:", process.env.REACT_APP_APPWRITE_PROJECT_ID);
+console.log("Endpoint:", process.env.REACT_APP_APPWRITE_ENDPOINT);
+
 const client = new Client();
 client
   .setEndpoint(`${process.env.REACT_APP_APPWRITE_ENDPOINT}`)
